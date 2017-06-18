1bd396354fb9a13df30dc04c1fd6b4472b012483
Mark Cola <m.cola@tlrg.com.au>
2017-06-18 11:55:01 +1000

    linked up home page

--- app/components/HomePage.js
+++ app/components/HomePage.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Text, View, StyleSheet, Button } from 'react-native';
 
 export default class HomePage extends Component {
-
   render() {
     return (
       <View style={styles.mainContainer}>
@@ -17,7 +16,6 @@ export default class HomePage extends Component {
               <Text style={styles.info}>kms ridden</Text>
             </View>
           </View>
-
           <View style={styles.secondStats}>
             <View style={styles.secondaryStat}>
               <Text style={[styles.count, styles.secondaryStat]}>$20.50</Text>
@@ -32,22 +30,23 @@ export default class HomePage extends Component {
               <Text style={styles.info}>campus emissions saved</Text>
             </View>
           </View>
-
           <View style={styles.todaysTip}>
             <Text style={styles.tipText}>
-              If you bike to Uni today you will save $3.80 in petrol.  If you bike all week, that's an extra $19 to spend on beer!
-                    </Text>
+              If you bike to Uni today you will save $3.80 in petrol. If you
+              bike all week, that's an extra $19 to spend on beer!
+            </Text>
           </View>
         </View>
         <View style={styles.bottomContent}>
           <View style={styles.todaysInfo}>
-            <Text style={styles.todaysInfoText}>Hey Jack, your facebook friend Jenny Delaney is biking today!</Text>
+            <Text style={styles.todaysInfoText}>
+              Hey Jack, your facebook friend Jenny Delaney is biking today!
+            </Text>
           </View>
           <View style={styles.logRide}>
-            <Button title="I'm biking today" backgroundColor='#2196f3'></Button>
+            <Button title="I'm biking today" backgroundColor="#2196f3" />
           </View>
         </View>
-
       </View>
     );
   }
@@ -55,44 +54,44 @@ export default class HomePage extends Component {
 
 const styles = StyleSheet.create({
   mainContainer: {
-    flex: 1
+    flex: 1,
   },
   topContent: {
-    flex: 2
+    flex: 2,
   },
   bottomContent: {
     flex: 1,
     flexDirection: 'column',
-    justifyContent: 'space-around'
+    justifyContent: 'space-around',
   },
   topStats: {
     flexDirection: 'row',
     flex: 0.8,
     justifyContent: 'space-around',
-    marginTop: 25
+    marginTop: 25,
   },
   mainStat: {
-    textAlign: 'center'
+    textAlign: 'center',
   },
   count: {
     fontWeight: 'bold',
     textAlign: 'center',
-    fontSize: 50
+    fontSize: 50,
   },
   secondStats: {
     flexDirection: 'row',
     flex: 0.5,
     justifyContent: 'space-around',
-    marginBottom: 20
+    marginBottom: 20,
   },
   secondaryStat: {
     textAlign: 'center',
     fontSize: 20,
-    width: 90
+    width: 90,
   },
   info: {
     textAlign: 'center',
-    color: '#444'
+    color: '#444',
   },
   todaysTip: {
     backgroundColor: '#bb285c',
@@ -101,13 +100,13 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'space-around',
     alignItems: 'center',
-    padding: 10
+    padding: 10,
   },
   tipText: {
-    color: "#fff",
+    color: '#fff',
     fontSize: 18,
     fontStyle: 'italic',
-    textAlign: 'center'
+    textAlign: 'center',
   },
   todaysInfo: {
     textAlign: 'center',
@@ -119,17 +118,15 @@ const styles = StyleSheet.create({
     borderBottomRightRadius: 0,
     borderWidth: 0.5,
     borderColor: '#d6d7da',
-    margin: 25
+    margin: 25,
   },
   todaysInfoText: {
     textAlign: 'center',
     paddingLeft: 13,
-    paddingRight: 13
+    paddingRight: 13,
   },
   logRide: {
     flex: 1,
-    justifyContent: 'flex-start'
-  }
-
+    justifyContent: 'flex-start',
+  },
 });
-
